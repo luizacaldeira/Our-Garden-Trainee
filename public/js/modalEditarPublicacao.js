bef5d58354a236f9c963889c420263f5a9c2042f
Hanna <hanna.velten@gmail.com>
2025-10-28 18:10:14 -0300

    adiciona função de trocar preview da imagem

--- public/js/modalEditarPublicacao.js
+++ public/js/modalEditarPublicacao.js
@@ -37,3 +37,24 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// FUNÇÃO PARA TROCAR PREVIEW DA IMAGEM
+function trocarPreviewImagem (idInput, idLabelContent){
+    if (idInput){
+        const input = document.getElementById(idInput);
+        const reader = new FileReader();
+        const imgSrc = input.files[0];
+
+        if (imgSrc){
+            reader.onload = (e) => {
+                const preview = document.getElementById(idLabelContent);
+                preview.innerHTML = `<img class="preview-image" src="${e.target.result}" alt="previsualização">`;
+            }
+
+            reader.onerror = (e) => {
+                console.error ("deu erro");
+            }
+
+            reader.readAsDataURL(imgSrc);
+        }
+    }
+}
