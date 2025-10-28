2b5e53ba624c6609a89ab3d0ac4851b644b43a95
Hanna <hanna.velten@gmail.com>
2025-10-28 17:23:49 -0300

    previne o envio do formulario ao clicar em qualquer botao

--- public/js/modalEditarPublicacao.js
+++ public/js/modalEditarPublicacao.js
@@ -1,15 +1,20 @@
+const fomulario = document.getElementById('editPublicationForm');
+fomulario.addEventListener('submit', (event) =>{
+    event.preventDefault();
+})
+
 const arrowUp = document.getElementById('arrowUpIcon');
 const arrowDown = document.getElementById('arrowDownIcon');
 
-function abrirDropdown (idDropdown,event){
-    console.log(event);
-    event.preventDefault();
+// FUNÇÃO QUE ABRE E FECHA O DROPDOWN
+function abrirDropdown (idDropdown){
     document.getElementById(idDropdown).classList.toggle('open');
 
     // arrowDown.style.display = 'none';
     // arrowUp.style.display = 'block';
 }
 
+// FUNÇÃO QUE LIMITA O MAXIMO DE 3 OPÇÕES DE ESCOLHA DE CLASSIFICAÇÃO
 document.addEventListener("DOMContentLoaded", () => {
     function limitCheckboxSelection(groupName, limit) {
     const checkboxes = document.querySelectorAll(`input[name="${groupName}"]`);
