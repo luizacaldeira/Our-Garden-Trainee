7bfc965e1ac59c480b2f79979b7cf2f9fef60f1b
DannyMattos <dayanne.mattos@estudante.ufjf.br>
2025-11-08 20:13:47 -0300

    JavaScript funcionando abrir e fechar

---
+++ public/js/sidebar.js
@@ -0,0 +1,14 @@
+    const divimagemlogo=document.getElementById('logo_ourgarden4');
+    document.getElementById('open_btn').addEventListener('click', function () {
+        const sidebar = document.getElementById('sidebar')
+        sidebar.classList.toggle('open-sidebar');
+        if (sidebar.classList.contains("open-sidebar")){
+        divimagemlogo.setAttribute("src", "/public/assets/1.png");
+        divimagemlogo.style.width="130px"
+        }
+        else{
+        divimagemlogo.setAttribute("src", "/public/assets/4.png");
+        divimagemlogo.style.width="50px"
+        }
+
+    });
