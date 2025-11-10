d91e640dc2efa4e7a379592ee79881110acc93f0
DannyMattos <dayanne.mattos@estudante.ufjf.br>
2025-11-10 10:12:12 -0300

    ajustes finais da logo, faltando js das abas e centralizar o "sair"

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,14 +1,14 @@
-    const divimagemlogo=document.getElementById('logo_ourgarden4');
-    // document.getElementById('open_btn').addEventListener('click', function () {
-    //     const sidebar = document.getElementById('sidebar')
-    //     sidebar.classList.toggle('open-sidebar');
-        if (sidebar.classList.contains("id_logo_our_garden")){
-        divimagemlogo.setAttribute("src", "/public/assets/1.png");
-        divimagemlogo.style.width="130px"
-        }
-        else{
-        divimagemlogo.setAttribute("src", "/public/assets/4.png");
-        divimagemlogo.style.width="50px"
-        }
+    // const divimagemlogo=document.getElementById('logo_ourgarden4');
+    // // document.getElementById('open_btn').addEventListener('click', function () {
+    // //     const sidebar = document.getElementById('sidebar')
+    // //     sidebar.classList.toggle('open-sidebar');
+    //     if (sidebar.classList.contains("id_logo_our_garden")){
+    //     divimagemlogo.setAttribute("src", "/public/assets/1.png");
+    //     divimagemlogo.style.width="130px"
+    //     }
+    //     else{
+    //     divimagemlogo.setAttribute("src", "/public/assets/4.png");
+    //     divimagemlogo.style.width="50px"
+    //     }
 
-    });
+    // });
