9a0b68e88bb3a139a7c943f33301779af51ab47c
DannyMattos <dayanne.mattos@estudante.ufjf.br>
2025-11-13 13:06:56 -0300

    arruma funcionalidade carrossel

---
+++ public/js/carrossel.js
@@ -0,0 +1,29 @@
+
+    const swiper = new Swiper('.swiper', {
+
+    direction: 'vertical',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 10,
+
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    watchOverflow: true,
+    mousewheel: true,
+    effect: "coverflow",         
+    grabCursor: true,            
+    centeredSlides: true,             
+    loop: true,                  
+    coverflowEffect: { 
+        rotate: 0,                
+        stretch: 0,                
+        depth: 0,                
+        modifier: 1,               
+        slideShadows: false,        
+    }, 
+}); 
+
