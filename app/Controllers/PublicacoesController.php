b315a7e7044e16d8b5aedbeb8eec495c2a74497d
Hanna <hanna.velten@gmail.com>
2025-11-13 12:20:22 -0300

    cria o listaPosts.view.php e o PublicacoesController

---
+++ app/Controllers/PublicacoesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class Controller
+{
+
+    public function index()
+    {
+        $posts = App::get('database')->selectAll('publicacoes');
+        return view('admin/listaPosts' ,compact('posts'));
+    }
+}
