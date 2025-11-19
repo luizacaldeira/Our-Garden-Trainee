536ec26b2f2f65785fac58f5379f6804b2003e3f
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-11-19 01:36:04 -0300

    finaliza ajustes da sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -4,15 +4,49 @@ const btnUsuarios = document.getElementById("btnUsuarios");
 const btnPublicacoes = document.getElementById("btnPublicacoes");
 const vazio = document.getElementById("vazio");
 
+const btnDashboardContainer= document.getElementById("btnDashboardContainer");
+const btnUsuariosContainer= document.getElementById("btnUsuariosContainer");
+const btnPublicacoesContainer = document.getElementById("btnPublicacoesContainer");
+
+btnDashboard.addEventListener("click", () => {
+    btnDashboard.classList.add('active');
+    
+    btnUsuarios.classList.remove('active');
+    btnPublicacoes.classList.remove('active');
+    
+    id_logo_ourgarden.style.borderBottomRightRadius = "20px";
+    btnUsuariosContainer.style.borderTopRightRadius = "20px";
+
+    btnUsuariosContainer.style.borderBottomRightRadius = "0px";
+    btnPublicacoesContainer.style.borderTopRightRadius = "0px";
+    vazio.style.borderTopRightRadius = "0px";
+
+});
+
 btnUsuarios.addEventListener("click", () => {
     btnUsuarios.classList.add('active');
     
     btnDashboard.classList.remove('active');
     btnPublicacoes.classList.remove('active');
+    
+    btnDashboardContainer.style.borderBottomRightRadius = "20px";
+    btnPublicacoesContainer.style.borderTopRightRadius = "20px";
 
     id_logo_ourgarden.style.borderBottomRightRadius = "0px";
     vazio.style.borderTopRightRadius = "0px";
+});
 
-    btnDashboard.style.borderBottomRightRadius = "50px";
-    btnPublicacoes.style.borderTopRightRadius = "50px";
-})
+btnPublicacoes.addEventListener("click", () => {
+    btnPublicacoes.classList.add('active');
+    
+    btnDashboard.classList.remove('active');
+    btnUsuarios.classList.remove('active');
+    
+    btnUsuariosContainer.style.borderBottomRightRadius = "20px";
+    vazio.style.borderTopRightRadius = "20px";
+
+    id_logo_ourgarden.style.borderBottomRightRadius = "0px";
+    btnPublicacoesContainer.style.borderTopRightRadius = "0px";
+    btnUsuariosContainer.style.borderTopRightRadius = "0px";
+    btnDashboardContainer.style.borderBottomRightRadius = "0px";
+});
