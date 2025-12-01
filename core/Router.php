64de133a2399fbd05d074dd6b612ae21a7680fe5
ViniciusBaylet <viniciusbaylet6@gmail.com>
2025-12-01 19:42:23 -0300

    arruma backend para lidar com rotas dinamicas

--- core/Router.php
+++ core/Router.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Core;
+
 use Exception;
 
 class Router
@@ -64,7 +65,34 @@ class Router
                 ...explode('@', $this->routes[$requestType][$uri])
             );
         }
+
+        // 2. Verifica rotas dinâmicas
+        foreach ($this->routes[$requestType] as $route => $controller) {
+            // Substitui {param} por regex para capturar valores
+            $pattern = preg_replace('/{[^}]+}/', '([^/]+)', $route);
+            if (preg_match("#^{$pattern}$#", $uri, $matches)) {
+                array_shift($matches); // Remove o match completo
+                $parts = explode('@', $controller);
+                return $this->callActionWithParams($parts[0], $parts[1], $matches);
+            }
+        }
+
         throw new Exception('No route defined for this URI.');
+    }
+
+    // Adicione este novo método:
+    protected function callActionWithParams($controller, $action, $params = [])
+    {
+        $controller = "App\Controllers\\" . $controller;
+        $controller = new $controller;
+
+        if (!method_exists($controller, $action)) {
+            throw new Exception(
+                "{$controller} does not respond to the {$action} action."
+            );
+        }
+
+        return call_user_func_array([$controller, $action], $params);
     }
 
     /**
